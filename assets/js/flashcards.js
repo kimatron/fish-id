c3bd2237a044065869c4fae988c061cb7d1cabf1
kimatron <kimmykim@gmail.com>
2023-09-20 23:07:41 +0100

    Add flackcards.html and .js file, and some test style. Start off code to test its functionality

---
+++ assets/js/flashcards.js
@@ -0,0 +1,22 @@
+const flashcards = [{
+    topic: 'Fish',
+    image: 'assets/images/barracuda.jpg',
+    imageAlt: 'barracuda in clear caribbean water',
+    word: 'Barracuda',
+    funfact: 'Hangs out on the surface keeping an eye out for his dinner',
+  },
+  {
+    topic: 'Invertebrates',
+    image: 'assets/images/arrowcrab.jpg',
+    imageAlt: 'close up photo of an arrowcrab',
+    word: 'Arrowcrab',
+    funfact: 'Keeps its food on his head, and snacks off it when he feels like it',
+  },
+  {
+    topic: 'Invertebrates',
+    image: 'assets/images/arrowcrab.jpg',
+    imageAlt: 'close up photo of an arrowcrab',
+    word: 'Arrowcrab',
+    funfact: 'Keeps its food on his head, and snacks off it when he feels like it',
+  },
+]
