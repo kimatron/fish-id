0a1ec7d15d3bc5d9c953a4fad12aafe33ca2cf49
kimatron <kimmykim@gmail.com>
2023-09-20 12:47:02 +0100

    Forgot to commit last night - fix index.html page to proper links, add some style to the css file, testing functionality of quiz code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-fish_data = [{
-    img: 'assets/images/vds.png',
+// Array containing quiz questions, photos, and correct answers
+quizData = [{
+    img: 'assets/images/jawfish.png',
     alt: 'cat-fish image in the ocean',
     options: [
       'cat-fish', 'dpg-fish', 'elephant-fish'
@@ -23,3 +24,49 @@ fish_data = [{
     correctAnswer: 'cat-fish'
   },
 ]
+
+const quizContainer = document.getElementById("quiz-container");
+const questionContainer = document.getElementById("question-container");
+const choicesContainer = document.getElementById("choices-container");
+const submitButton = document.getElementById("submit-button");
+const resultContainer = document.getElementById("result-container");
+
+let currentQuestion = 0;
+let score = 0;
+
+function loadQuestion() {
+  const currentQuizData = quizData[currentQuestion];
+
+  questionContainer.innerHTML = currentQuizData.question;
+  choicesContainer.innerHTML = "";
+
+  currentQuizData.choices.forEach((choice, index) => {
+    const choiceElement = document.createElement("button");
+    choiceElement.innerHTML = choice;
+    choiceElement.addEventListener("click", () =>
+      selectAnswer(index)
+    );
+    choicesContainer.appendChild(choiceElement);
+  });
+}
+
+function selectAnswer(answerIndex) {
+  if (answerIndex === quizData[currentQuestion].correctAnswer) {
+    score++;
+  }
+
+  currentQuestion++;
+
+  if (currentQuestion < quizData.length) {
+    loadQuestion();
+  } else {
+    showResult();
+  }
+}
+
+function showResult() {
+  quizContainer.style.display = "none";
+  resultContainer.innerHTML = `You scored ${score} out of ${quizData.length} questions.`;
+}
+
+loadQuestion();
