0308dc0c93829e9bc38508c5c8c47369b651fa40
kimatron <kimmykim@gmail.com>
2023-09-26 16:40:00 +0100

    Add border to question container, add footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-/** Script for modal */
+/**
+ * This function is responsible for displaying the modal.
+ * By setting the "display" property of the element with the id "myModal" to "block". 
+ */
 function openModal() {
   document.getElementById("myModal").style.display = "block";
 }
-
+/**
+ * This function is responsible for hiding the modal.
+ * By setting the "display" property of the element with the id "myModal" to "none". 
+ */
 function closeModal() {
   document.getElementById("myModal").style.display = "none";
 }
